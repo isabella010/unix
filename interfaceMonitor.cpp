88240e4e05fe091ccccef2553d39e63d9e50ae2a
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-21 18:38:40 -0400

    Update interfaceMonitor.cpp

--- interfaceMonitor.cpp
+++ interfaceMonitor.cpp
@@ -1,137 +1,205 @@
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <vector>
-#include <signal.h>
+#include <cstring>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <cstring>
-#include <cstdlib>
-#include <algorithm>
-#include <sys/select.h>
+#include <net/if.h>
+#include <csignal>
+#include <errno.h>
 
 #define SOCKET_PATH "/tmp/interfaceMonitor.socket"
 #define BUF_SIZE 200
 
 using namespace std;
 
-int serverFd;
-vector<int> clientFds;
-vector<string> interfaces;
+bool is_running = true;
+int sockfd = -1;
+string interface;
 
-void handleSigint(int sig) {
-    for (int fd : clientFds) {
-        write(fd, "Shut Down", 10);
-    }
-    close(serverFd);
-    unlink(SOCKET_PATH);
-    for (int fd : clientFds) {
-        close(fd);
-    }
-    exit(0);
+void handle_sigint(int sig) {
+    is_running = false;
 }
 
-int main() {
-    signal(SIGINT, handleSigint);
+int set_if_flags(const char *ifname, short flags) {
+    struct ifreq ifr;
+    int res = 0;
+    strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
+    ifr.ifr_flags = flags;
 
-    struct sockaddr_un name;
+    int skfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (skfd < 0) {
+        perror("socket error");
+        return 1;
+    }
+
+    res = ioctl(skfd, SIOCSIFFLAGS, &ifr);
+    if (res < 0) {
+        perror("ioctl error");
+    }
+    close(skfd);
+    return res;
+}
+
+int set_if_up(const char *ifname) {
+    return set_if_flags(ifname, IFF_UP | IFF_RUNNING);
+}
+
+void get_interface_stat(const string &interface, string &data) {
+    string operstate;
+    int carrier_up_count = 0;
+    int carrier_down_count = 0;
+    int tx_bytes = 0;
+    int rx_bytes = 0;
+    int rx_dropped = 0;
+    int rx_errors = 0;
+    int tx_packets = 0;
+    int rx_packets = 0;
+    int tx_dropped = 0;
+    int tx_errors = 0;
+    char statPath[BUF_SIZE*2];
+    ifstream infile;
+
+    sprintf(statPath, "/sys/class/net/%s/operstate", interface.c_str());
+    infile.open(statPath);
+    if (infile.is_open()) {
+        infile >> operstate;
+        infile.close();
+    }
+
+    sprintf(statPath, "/sys/class/net/%s/carrier_up_count", interface.c_str());
+    infile.open(statPath);
+    if (infile.is_open()) {
+        infile >> carrier_up_count;
+        infile.close();
+    }
+
+    sprintf(statPath, "/sys/class/net/%s/carrier_down_count", interface.c_str());
+    infile.open(statPath);
+    if (infile.is_open()) {
+        infile >> carrier_down_count;
+        infile.close();
+    }
+
+    sprintf(statPath, "/sys/class/net/%s/statistics/tx_bytes", interface.c_str());
+    infile.open(statPath);
+    if (infile.is_open()) {
+        infile >> tx_bytes;
+        infile.close();
+    }
+
+    sprintf(statPath, "/sys/class/net/%s/statistics/rx_bytes", interface.c_str());
+    infile.open(statPath);
+    if (infile.is_open()) {
+        infile >> rx_bytes;
+        infile.close();
+    }
+
+    sprintf(statPath, "/sys/class/net/%s/statistics/rx_dropped", interface.c_str());
+    infile.open(statPath);
+    if (infile.is_open()) {
+        infile >> rx_dropped;
+        infile.close();
+    }
+
+    sprintf(statPath, "/sys/class/net/%s/statistics/rx_errors", interface.c_str());
+    infile.open(statPath);
+    if (infile.is_open()) {
+        infile >> rx_errors;
+        infile.close();
+    }
+
+    sprintf(statPath, "/sys/class/net/%s/statistics/tx_packets", interface.c_str());
+    infile.open(statPath);
+    if (infile.is_open()) {
+        infile >> tx_packets;
+        infile.close();
+    }
+
+    sprintf(statPath, "/sys/class/net/%s/statistics/tx_dropped", interface.c_str());
+    infile.open(statPath);
+    if (infile.is_open()) {
+        infile >> tx_dropped;
+        infile.close();
+    }
+
+    sprintf(statPath, "/sys/class/net/%s/statistics/tx_errors", interface.c_str());
+    infile.open(statPath);
+    if (infile.is_open()) {
+        infile >> tx_errors;
+        infile.close();
+    }
+
+    sprintf(statPath, "/sys/class/net/%s/statistics/rx_packets", interface.c_str());
+    infile.open(statPath);
+    if (infile.is_open()) {
+        infile >> rx_packets;
+        infile.close();
+    }
+
+    if (operstate == "down") {
+        set_if_up(interface.c_str());
+    }
+
+    data = "Interface: " + interface + " state: " + operstate + " up_count: " + to_string(carrier_up_count) + " down_count: " + to_string(carrier_down_count) + "\n" +
+           "rx_bytes: " + to_string(rx_bytes) + " rx_dropped: " + to_string(rx_dropped) + " rx_errors: " + to_string(rx_errors) + " rx_packets: " + to_string(rx_packets) + "\n" +
+           "tx_bytes: " + to_string(tx_bytes) + " tx_dropped: " + to_string(tx_dropped) + " tx_errors: " + to_string(tx_errors) + " tx_packets: " + to_string(tx_packets) + "\n\n";
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <interface>" << endl;
+        return EXIT_FAILURE;
+    }
+
     char buffer[BUF_SIZE];
+    signal(SIGINT, handle_sigint);
+    interface = argv[1];
 
-    int numInterfaces;
-    cout << "Enter # interfaces to monitor: ";
-    cin >> numInterfaces;
-
-    for (int i = 0; i < numInterfaces; ++i) {
-        string interface;
-        cout << "Enter name of interface " << i + 1 << ": ";
-        cin >> interface;
-        interfaces.push_back(interface);
+    struct sockaddr_un addr;
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("socket error");
+        return EXIT_FAILURE;
     }
 
-    serverFd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (serverFd < 0) {
-        perror("socket");
-        exit(-1);
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+
+    while (connect(sockfd, (struct sockaddr*)&addr, sizeof(struct sockaddr_un)) < 0) {
+        sleep(1);
     }
 
-    memset(&name, 0, sizeof(struct sockaddr_un));
-    name.sun_family = AF_UNIX;
-    strncpy(name.sun_path, SOCKET_PATH, sizeof(name.sun_path) - 1);
+    cout << "Connected to server" << endl; // Debug output
 
-    if (bind(serverFd, (struct sockaddr*)&name, sizeof(name)) < 0) {
-        perror("bind");
-        exit(-1);
-    }
+    write(sockfd, "Ready", 5);
 
-    if (listen(serverFd, numInterfaces) < 0) {
-        perror("listen");
-        exit(-1);
-    }
-
-    cout << "Waiting for connections..." << endl;
-
-    fd_set active_fd_set;
-    fd_set read_fd_set;
-    FD_ZERO(&active_fd_set);
-    FD_SET(serverFd, &active_fd_set);
-    int max_fd = serverFd;
-
-    while (true) {
-        read_fd_set = active_fd_set;
-        if (select(max_fd + 1, &read_fd_set, NULL, NULL, NULL) < 0) {
-            perror("select");
-            break;
-        }
-
-        for (int i = 0; i <= max_fd; ++i) {
-            if (FD_ISSET(i, &read_fd_set)) {
-                if (i == serverFd) {
-                    int clientFd = accept(serverFd, NULL, NULL);
-                    if (clientFd < 0) {
-                        perror("accept");
-                    } else {
-                        FD_SET(clientFd, &active_fd_set);
-                        if (clientFd > max_fd) {
-                            max_fd = clientFd;
-                        }
-                        clientFds.push_back(clientFd);
-                        cout << "Accepted connection from client" << endl;
-                    }
-                } else {
-                    int ret = read(i, buffer, BUF_SIZE);
-                    if (ret <= 0) {
-                        if (ret < 0) {
-                            perror("read");
-                        }
-                        close(i);
-                        FD_CLR(i, &active_fd_set);
-                        clientFds.erase(remove(clientFds.begin(), clientFds.end(), i), clientFds.end());
-                        cout << "Client disconnected" << endl;
-                    } else {
-                        buffer[ret] = '\0';
-                        cout << "Received from client: " << buffer << endl; // Debug output
-                        if (strncmp(buffer, "Ready", 5) == 0) {
-                            write(i, "Monitor", 7);
-                        } else if (strncmp(buffer, "Link Down", 9) == 0) {
-                            write(i, "Set Link Up", 11);
-                        } else if (strncmp(buffer, "Done", 4) == 0) {
-                            close(i);
-                            FD_CLR(i, &active_fd_set);
-                            clientFds.erase(remove(clientFds.begin(), clientFds.end(), i), clientFds.end());
-                            cout << "Client marked as done" << endl;
-                        }
-                    }
+    while (is_running) {
+        int bytes_read = read(sockfd, buffer, BUF_SIZE);
+        if (bytes_read > 0) {
+            buffer[bytes_read] = '\0';
+            cout << "Received command: " << buffer << endl; // Debug output
+            if (strncmp(buffer, "Monitor", 7) == 0) {
+                while (is_running) {
+                    string data;
+                    get_interface_stat(interface, data);
+                    write(sockfd, data.c_str(), data.size());
+                    sleep(1);
                 }
+            } else if (strncmp(buffer, "Set Link Up", 11) == 0) {
+                set_if_up(interface.c_str());
+                write(sockfd, "Link Up", 7);
+            } else if (strncmp(buffer, "Shut Down", 9) == 0) {
+                is_running = false;
+                write(sockfd, "Done", 4);
+                break;
             }
         }
     }
 
-    close(serverFd);
-    unlink(SOCKET_PATH);
-    for (int fd : clientFds) {
-        close(fd);
-    }
-
+    close(sockfd);
     return 0;
 }
