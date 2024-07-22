a974223822c02454a155b46c797209bcb6a9201f
Isabella Burin <105743002+isabella010@users.noreply.github.com>
2024-07-22 19:32:38 -0400

    Update networkMonitor.cpp

--- networkMonitor.cpp
+++ networkMonitor.cpp
@@ -6,28 +6,32 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <algorithm>
-#include <sys/select.h>
 #include <cstring>
 #include <cstdlib>
+#include <algorithm>
+#include <sys/select.h>
+
+#define SOCKET_PATH "/tmp/interfaceMon"
+#define BUF_SIZE 200
 
 using namespace std;
-
-#define SOCKET_PATH "/tmp/interfaceMonitor.socket"
-#define BUF_SIZE 200
-int serverFd;
 vector<int> clientFds;
 vector<string> interfaces;
 
-void handleSigint(int sig) {
-    for (int fd : clientFds) {
-        write(fd, "Shut Down", 10);
+int serverFd;
+
+void handleSigint(int sigint){ //void handleSigint(int sig){
+    for(auto i = 0; i < clientFds.size(); ++i){
+        write(clientFds[i], "shut down", 10);
     }
+    
     close(serverFd);
     unlink(SOCKET_PATH);
-    for (int fd : clientFds) {
-        close(fd);
+    
+    for(auto i = 0; i < clientFds.size(); ++i){
+        close(clientFds[i]);
     }
+    
     exit(0);
 }
 
@@ -37,11 +41,11 @@ int main() {
     struct sockaddr_un name;
     char buffer[BUF_SIZE];
 
-    int numInterfaces;
-    cout << "Enter # interfaces to monitor: ";
-    cin >> numInterfaces;
+    int interfaceCount;
+    cout << "enter # interfaces to monitor: ";
+    cin >> interfaceCount;
 
-    for (int i = 0; i < numInterfaces; ++i) {
+    for (int i = 0; i < interfaceCount; ++i) {
         string interface;
         cout << "Enter name of interface " << i + 1 << ": ";
         cin >> interface;
@@ -63,7 +67,7 @@ int main() {
         exit(-1);
     }
 
-    if (listen(serverFd, numInterfaces) < 0) {
+    if(listen(serverFd, interfaceCount) < 0){
         perror("listen");
         exit(-1);
     }
@@ -76,52 +80,65 @@ int main() {
     FD_SET(serverFd, &active_fd_set);
     int max_fd = serverFd;
 
-    for (const string &interface : interfaces) {
-        if (fork() == 0) {
+    for(auto i = 0; i < interfaces.size(); ++i){
+        const string &interface = interfaces[i];
+        
+        if(fork() == 0){
             execl("./interfaceMon", "./interfaceMon", interface.c_str(), NULL);
             exit(0);
         }
     }
 
-    while (true) {
+    while(true){
         read_fd_set = active_fd_set;
-        if (select(max_fd + 1, &read_fd_set, NULL, NULL, NULL) < 0) {
+        
+        if(select(max_fd + 1, &read_fd_set, NULL, NULL, NULL) <0){
             perror("select");
             break;
         }
 
-        for (int i = 0; i <= max_fd; ++i) {
-            if (FD_ISSET(i, &read_fd_set)) {
+        for(int i = 0; i <= max_fd; ++i){
+            if (FD_ISSET(i, &read_fd_set)){
                 if (i == serverFd) {
                     int clientFd = accept(serverFd, NULL, NULL);
                     if (clientFd < 0) {
                         perror("accept");
-                    } else {
+                    } 
+                    else{
                         FD_SET(clientFd, &active_fd_set);
-                        if (clientFd > max_fd) {
+                        
+                        if(clientFd > max_fd){
                             max_fd = clientFd;
                         }
+                        
                         clientFds.push_back(clientFd);
-                        cout << "Accepted connection from client" << endl;
+                        cout << "Accepted conection from client" << endl;
                     }
-                } else {
+                } 
+                else {
                     int ret = read(i, buffer, BUF_SIZE);
-                    if (ret <= 0) {
-                        if (ret < 0) {
+                    
+                    if(ret <= 0){
+                        if (ret < 0){
                             perror("read");
                         }
+                        
                         close(i);
                         FD_CLR(i, &active_fd_set);
                         clientFds.erase(remove(clientFds.begin(), clientFds.end(), i), clientFds.end());
                         cout << "Client disconnected" << endl;
-                    } else {
+                    } 
+                    else{
                         buffer[ret] = '\0';
-                        cout << "Received from client: " << buffer << endl;
+                        cout << buffer << endl;
+                        
                         if (strncmp(buffer, "Ready", 5) == 0) {
                             write(i, "Monitor", 7);
-                        } else if (strncmp(buffer, "Link Down", 9) == 0) {
+                        } 
+                        else if (strncmp(buffer, "Link Down", 9) == 0) {
                             write(i, "Set Link Up", 11);
-                        } else if (strncmp(buffer, "Done", 4) == 0) {
+                        } 
+                        else if (strncmp(buffer, "Done", 4) == 0) {
                             close(i);
                             FD_CLR(i, &active_fd_set);
                             clientFds.erase(remove(clientFds.begin(), clientFds.end(), i), clientFds.end());
@@ -135,8 +152,8 @@ int main() {
 
     close(serverFd);
     unlink(SOCKET_PATH);
-    for (int fd : clientFds) {
-        close(fd);
+    for (auto i = 0; i < clientFds.size(); ++i) {
+        close(clientFds[i]);
     }
 
     return 0;
